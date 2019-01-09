c453fd283ed9553132e723460422c597182e70f1
davejmacm <davejmacm@gmail.com>
2019-01-09 14:15:04 +0000

    console created

---
+++ db/console.rb
@@ -0,0 +1,17 @@
+require("pry")
+require_relative("../models/albums")
+require_relative("../models/artists")
+
+artist1 = Artist.new({'name' => 'David Bowie'})
+artist1.save()
+
+album1 = Album.new({
+  'title' => 'Greatest Hits',
+  'genre' => 'Rock'
+  'artist_id' => artist1.id
+  })
+  album1.save()
+
+
+  binding.pry
+  nil
